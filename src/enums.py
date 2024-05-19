3c23821c146dfdd8debcf1abf48f3325f335c8b4
Eldos <eldos.baktybekov@gmail.com>
2024-05-19 16:26:44 +0600

    :label: Add OrderState enum and change SalesChannel enum to IntEnum

--- src/enums.py
+++ src/enums.py
@@ -1,13 +1,22 @@
-from enum import StrEnum, auto
+from enum import IntEnum, StrEnum, auto
 
-__all__ = ('CountryCode', 'SalesChannel')
+__all__ = ('CountryCode', 'SalesChannel', 'OrderState')
 
 
 class CountryCode(StrEnum):
     RU = auto()
 
 
-class SalesChannel(StrEnum):
-    DINE_IN = auto()
-    TAKEAWAY = auto()
-    DELIVERY = auto()
+class SalesChannel(IntEnum):
+    DINE_IN = 3
+    TAKEAWAY = 2
+    DELIVERY = 1
+
+
+class OrderState(IntEnum):
+    ACCEPTED = 1
+    IN_PROGRESS = 2
+    PACKED = 3
+    DELIVERY = 4
+    LATE = 11
+    CANCELED = 12
