71a6c7f3c50a1699888bb91ee798fa0b7f2ecfe3
Eldos <eldos.baktybekov@gmail.com>
2024-05-20 00:26:22 +0600

    :label: Improve type hints

--- src/connections/helpers.py
+++ src/connections/helpers.py
@@ -1,7 +1,7 @@
 import asyncio
 import functools
 from collections.abc import Callable
-from typing import ParamSpec, TypeAlias, TypeVar
+from typing import ParamSpec, TypeVar
 
 import structlog.stdlib
 
@@ -12,20 +12,18 @@ logger = structlog.stdlib.get_logger('app')
 T = TypeVar("T")
 P = ParamSpec('P')
 
-Callback: TypeAlias = Callable[P, T]
 
-
-def retry_on_failure(attempts: int) -> Callable[[Callback], Callback]:
+def retry_on_failure(attempts: int) -> Callable[[Callable[P, T]], Callable[P, T]]:
     if attempts < 1:
         raise ValueError("Attempts must be greater than 0")
 
-    def decorator(func: Callable) -> Callable:
+    def decorator(func: Callable[P, T]) -> Callable[P, T]:
 
         if not asyncio.iscoroutinefunction(func):
             raise ValueError('Function must be async')
 
         @functools.wraps(func)
-        async def wrapper(*args, **kwargs) -> T:
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             for _ in range(attempts):
                 try:
                     return await func(*args, **kwargs)
