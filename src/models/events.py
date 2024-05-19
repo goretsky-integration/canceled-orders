b5ebe9d07528cfef9d3dc679e5d2fc3b85fc2906
Eldos <eldos.baktybekov@gmail.com>
2024-05-20 00:34:48 +0600

    :label: Update event payload structure

--- src/models/events.py
+++ src/models/events.py
@@ -1,14 +1,23 @@
+from uuid import UUID
+
 from pydantic import BaseModel, Field
 
-from enums import CountryCode
-from models import DetailedOrder
+from enums import CountryCode, SalesChannel
 
-__all__ = ('EventPayload', 'Event')
+__all__ = ('EventPayload', 'Event', 'EventPayloadOrder')
+
+
+class EventPayloadOrder(BaseModel):
+    id: UUID
+    number: str
+    price: int
+    sales_channel: SalesChannel
+    is_refund_receipt_printed: bool
 
 
 class EventPayload(BaseModel):
     unit_name: str
-    orders: list[DetailedOrder]
+    orders: list[EventPayloadOrder]
     country_code: CountryCode
 
 
