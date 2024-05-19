8532553a24b916b12fe5f7278cbb1eccbcf7ee5a
Eldos <eldos.baktybekov@gmail.com>
2024-05-20 00:35:13 +0600

    :label: Update partial order model structure

--- src/models/partial_orders.py
+++ src/models/partial_orders.py
@@ -1,15 +1,31 @@
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from enums import SalesChannel
-
-__all__ = ('PartialOrder',)
+__all__ = (
+    'PartialOrder',
+    'PartialOrdersResponsePagination',
+    'PartialOrdersResponse',
+)
 
 
 class PartialOrder(BaseModel):
-    id: UUID
-    price: int
-    number: str
-    sales_channel: SalesChannel
-    account_name: str
+    id: Annotated[UUID, Field(alias='Id')]
+
+
+class PartialOrdersResponsePagination(BaseModel):
+    current_page: Annotated[int, Field(alias='CurrentPage')]
+    next_page: Annotated[int | None, Field(alias='NextPage')]
+    per_page: Annotated[int, Field(alias='PerPage')]
+    prev_page: Annotated[int | None, Field(alias='PrevPage')]
+    total_pages: Annotated[int, Field(alias='TotalPages')]
+    total_records: Annotated[int, Field(alias='TotalRecords')]
+
+
+class PartialOrdersResponse(BaseModel):
+    items: Annotated[list[PartialOrder], Field(alias='Items')]
+    pagination: Annotated[
+        PartialOrdersResponsePagination,
+        Field(alias='Pagination'),
+    ]
